3e9727012f212aca4ed47b76385ecdb88318510f
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-07-25 16:06:46 +0900

    update 2022.07.25

--- src/main/java/bbs/BbsDAO.java
+++ src/main/java/bbs/BbsDAO.java
@@ -96,6 +96,7 @@ public class BbsDAO {
 				bbs.setBbsDate(rs.getString(4));
 				bbs.setBbsContent(rs.getString(5));
 				bbs.setBbsAvailable(rs.getInt(6));
+				list.add(bbs);
 			}
 					
 		}catch(Exception e) {
@@ -119,5 +120,58 @@ public class BbsDAO {
 			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	//하나의 게시글을 보는 메소드
+	public Bbs getBbs(int bbsID) {
+		String sql = "select * from bbs where bbsID = ?";
+		try {
+			PreparedStatement pstmt =conn.prepareStatement(sql);
+			pstmt.setInt(1, bbsID);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				return bbs;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	//게시글 수정 메소드
+	public int update(int bbsID, String bbsTitle, String bbsContent) {
+		String sql = "update bbs set bbsTitle = ?, bbsContent =? where bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, bbsTitle);
+			pstmt.setString(2, bbsContent);
+			pstmt.setInt(3, bbsID);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return -1; // 데이터베이스 오류
+	}
+	
+	//게시글 삭제 메소드
+	public int delete(int bbsID) {
+		//실제 데이터를 삭제하는 것이 아니라 게시글 유효숫자를 '0' 으로 수정한다
+		String sql = "update bbs set bbsAvailable = 0 where bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bbsID);
+			return pstmt.executeUpdate();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
 	}
 }
